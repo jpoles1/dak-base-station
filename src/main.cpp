0a026876065b999edae4be9ec744340a88ac3304
Jordan Poles <jpoles1@gmail.com>
2018-10-02 09:44:58 -0400

    DAK Servo blind controls working

---
+++ src/main.cpp
@@ -0,0 +1,120 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include "ESP8266WiFi.h"
+#include <WebSocketClient.h>
+
+#include <Arduino.h>
+
+//Private Config
+#include "config.h"
+
+//Pins
+#define SERVO_PIN D0
+#define LED_PIN D3
+
+//Global variables
+WebSocketClient webSocketClient;
+WiFiClient client;
+Servo contServo;
+bool windowState = 0;
+
+void wifiCheck(){
+    if(WiFi.status() != WL_CONNECTED){
+        Serial.print("Connecting to WiFi...");
+    }
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.println("");
+}
+void websocketLoad() {
+    if (client.connect(WEBSOCKET_URL, 80)) {
+        Serial.println("Connected");
+    } else {
+        Serial.println("Connection failed.");
+    }
+    
+    webSocketClient.path = "/";
+    webSocketClient.host = strdup(WEBSOCKET_URL);
+    if (webSocketClient.handshake(client)) {
+        Serial.println("Handshake successful");
+    } else {
+        Serial.println("Handshake failed.");
+    }
+}
+
+void turnServo(int speed, int duration){
+    contServo.attach(SERVO_PIN);
+    contServo.write(speed);
+    delay(duration);
+    contServo.write(90);
+    contServo.detach();
+}
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
+    WiFi.begin(AP_SSID, AP_PASSWORD);
+    wifiCheck();    
+    websocketLoad();
+    digitalWrite(LED_PIN, LOW);
+}
+
+std::vector<String> splitStringToVector(String msg, char delim){
+  std::vector<String> subStrings;
+  int j=0;
+  for(int i =0; i < msg.length(); i++){
+    if(msg.charAt(i) == delim){
+      subStrings.push_back(msg.substring(j,i));
+      j = i+1;
+    }
+  }
+  subStrings.push_back(msg.substring(j,msg.length())); //to grab the last value of the string
+  return subStrings;
+}
+
+void openWindow(){
+    Serial.println("Opening Window!");
+    if(windowState  == 0) {
+        turnServo(180, 3000);
+        windowState = 1;
+    }
+}
+
+void closeWindow(){
+    Serial.println("Closing Window!");
+    if(windowState  == 1) {
+        turnServo(0, 3000);
+        windowState = 0;
+    }
+}
+
+String data;
+void loop() {
+    if (client.connected()) {
+        webSocketClient.getData(data);
+        if (data.length() > 0) {
+            Serial.print("Received data: ");
+            Serial.println(data);
+            std::vector<String> split_string = splitStringToVector(data.c_str(), ':');
+            String msgType = split_string[0];
+            String deviceName = split_string[1];
+            String fname = split_string[2];
+            String command = split_string[3];
+            if(deviceName == "window" && fname == "power"){
+                if(command == "off"){
+                    closeWindow();
+                }
+                if(command == "on"){
+                    openWindow();
+                }
+            }
+        }
+        data = "";
+    } else {
+        Serial.println("Client disconnected.");
+        websocketLoad();
+    }
+}
