94ebc4aee4a8271c55606d80752d1d2c714913f7
Jordan Poles <jpoles1@gmail.com>
2019-04-25 17:39:19 -0400

    Adding in PEAP authentication

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include "config.h"
 
 //Libs
-#include "WiFi.h"
+#include <WiFi.h>
+#include "esp_wpa2.h"
 #include <WebSocketClient.h>
 #include "RCSwitch.h"
 #include <oled_log.h>
@@ -19,9 +20,14 @@ void wifiCheck(){
     if(WiFi.status() != WL_CONNECTED){
         displayString("Connecting to WiFi...", false);
     }
+    int counter = 0;
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         displayString(".", false);
+        counter++;
+        if(counter>=60){ //after 30 seconds timeout - reset board
+            ESP.restart();
+        }
     }
     displayString(" ");
 }
@@ -56,9 +62,17 @@ void setup() {
     transmitter433.setPulseLength(140);
     transmitter433.setRepeatTransmit(5);
     //Setup wifi
-    uint8_t new_mac[8] = {0x84,0x3A,0x4B,0xC8,0xE9,0x00};
-    esp_base_mac_addr_set(new_mac);
-    WiFi.begin(AP_SSID, AP_PASSWORD);
+    //uint8_t new_mac[8] = {0x39,0xAE,0xA4,0x16,0x1F,0x08};
+    //esp_base_mac_addr_set(new_mac);
+    WiFi.disconnect(true);  //disconnect form wifi to set new wifi connection
+    WiFi.mode(WIFI_STA); //init wifi mode
+    esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY)); //provide identity
+    esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY)); //provide username --> identity and username is same
+    esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD)); //provide password
+    esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT(); //set config settings to default
+    esp_wifi_sta_wpa2_ent_enable(&config); //set config settings to enable function
+    WiFi.begin(AP_SSID);
+    //WiFi.setHostname("ESPDAK");
     websocketLoad();
 }
 
